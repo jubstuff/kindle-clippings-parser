8dc1772ddd3b909ea9b933ff105fb829178744df
Giustino Borzacchiello <giustinob@gmail.com>
2017-01-30 22:47:15 +0100

    Get position in Italian

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-var through = require('through');
-var split = require('split');
+var through = require( 'through' );
+var split = require( 'split' );
 
 var SECTION_SEP_PATTERN = '==========';
 
-module.exports = function (stream) {
+module.exports = function ( stream ) {
   return stream
-    .pipe(split(SECTION_SEP_PATTERN))
-    .pipe(through(function write (section) {
+    .pipe( split( SECTION_SEP_PATTERN ) )
+    .pipe( through( function write( section ) {
       section = section.trim();
-      if (!section) return;
+      if ( ! section ) {
+        return;
+      }
 
-      var lines = section.split(/\r?\n/);
+      var lines = section.split( /\r?\n/ );
       var metas = lines[1];
       var json = {
         title: lines[0].trim(),
-        content: lines.slice(2).join('\n').trim(),
-        type: metas.match(/- (.+?) .*?\|/)[1],
-        loc: metas.match(/(?:Loc\.|Page)(.+)\|/)[1].trim(),
-        date: metas.match(/ on (.+)/)[1]
+        content: lines.slice( 2 ).join( '\n' ).trim(),
+        type: metas.match( /- (.+?) .*?\|/ )[1],
+        pos: metas.match( /(?:Pos\.)(.+)\|/ )[1].trim(),
+        date: metas.match( / il (.+)/ )[1]
       };
-      this.emit('data', json);
-    }));
+      this.emit( 'data', json );
+    } ) );
 };
